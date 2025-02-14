ea896fce415d8d0fbf76be668bc5a82e1df30859
Prasanth K S <ksprasanthksp10@gmail.com>
2025-02-14 13:03:16 +0530

    (14.02.2025) FN Basics of JS

---
+++ JavaScript/operators.js
@@ -0,0 +1,37 @@
+/*
+// Logical operator
+console.log("Logical AND operator");
+console.log(5 && 10);
+console.log(true && 10);
+console.log(false && 10);
+console.log(false && true);
+console.log(true && false);
+
+console.log("Logical OR operator");
+console.log(5 || 10); //15
+console.log(true || 10); //15
+console.log(false || 10); //10
+console.log(false || true); // true
+console.log(true || false); // true
+*/
+
+//Relational Operator
+/*
+a = '5'
+b = 5
+console.log("This is == operator",a == b);
+console.log("This is === operator",a === b);
+
+a = '4'
+b = 5
+console.log("This is != operator",a != b);
+console.log("This is !== operator",a !== b);
+*/
+
+a = 70
+b = 35
+console.log(a&b);
+console.log(a^b);
+
+c = 1
+console.log(~c);
