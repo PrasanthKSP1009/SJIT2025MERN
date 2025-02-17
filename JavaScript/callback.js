9507ff6adabfb95b4c45b07eee7573e94138d99a
Prasanth K S <ksprasanthksp10@gmail.com>
2025-02-17 16:59:03 +0530

    (17.02.2025) AN Advanced CSS Started and React Project Started

--- JavaScript/callback.js
+++ JavaScript/callback.js
@@ -1,3 +1,4 @@
+/*
 function SJITCollege(message,abc){
     console.log(message);
     abc()
@@ -6,3 +7,17 @@ function callbackSJIT(){
     console.log("Welcome back to SJIT");
 }
 SJITCollege("Welcome to SJIT",callbackSJIT)
+*/
+
+function formSubmission(cbFun){
+    if(cbFun())
+        console.log("Form Submitted Successfully")
+    else{
+        console.log("Form Submission Unsuccessful");
+    }
+}
+function formValidation(){
+    console.log("Form Validation Successful");
+    return true
+}
+formSubmission(formValidation)
