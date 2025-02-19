b6d4da9454a97b53e28479f848829350abc7b959
Prasanth K S <ksprasanthksp10@gmail.com>
2025-02-19 13:21:14 +0530

    (19.02.2025) FN React Basics Completed and Routers Started

--- sjitFrontend/src/App.jsx
+++ sjitFrontend/src/App.jsx
@@ -1,27 +1,26 @@
-import Home from './components/FunctionalComponents/Home'
-import About from './components/FunctionalComponents/About'
-import Gallery from './components/FunctionalComponents/Gallery'
-import Contact from './components/FunctionalComponents/Contact'
-import Signup from './components/FunctionalComponents/Signup'
+import Home from "./components/FunctionalComponents/Home";
+import About from "./components/FunctionalComponents/About";
+import Gallery from "./components/FunctionalComponents/Gallery";
+import Contact from "./components/FunctionalComponents/Contact";
+import Signup from "./components/FunctionalComponents/Signup";
+import Navbar from "./components/FunctionalComponents/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <hr />
-      <h1>Welcome to React</h1>
-      <hr />
-      <Home />
-      <hr />
-      <About />
-      <hr />
-      <Gallery />
-      <hr />
-      <Contact />
-      <hr />
-      <Signup />
-      <hr />
-    </div>
-  )
+    <main>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home properties="Hello" sjit="SJIT" />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- sjitFrontend/src/components/FunctionalComponents/Home.jsx
+++ sjitFrontend/src/components/FunctionalComponents/Home.jsx
@@ -1,7 +1,14 @@
-function Home() {
+function Home(properties) {
+  var styling = {
+    textAlign:"center",
+    backgroundColor:"blue",
+    color:"white"
+  }
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1 style={styling}>Home Page</h1>
+      <h2>Below tag is for props</h2>
+      <h3>Props: {properties.properties}, {properties.sjit}</h3>
     </div>
   );
 }
