e51e425896df33c0ad270026ecc7c21be114d73b
Prasanth K S <ksprasanthksp10@gmail.com>
2025-02-25 13:45:46 +0530

    (25.02.2025) Backend to MongoDB integration Successful

--- sjitBackend/index.js
+++ sjitBackend/index.js
@@ -1,16 +1,49 @@
 const express = require("express");
-
+const mdb = require("mongoose");
+const dotenv = require("dotenv");
+const Signup = require("./models/signupSchema");
 const app = express();
-
-app.get("/",(req,res)=>{
-    res.send("<h1>Welcome to Backend</h1>")
-})
-app.get("/static",(req,res)=>{
-    res.sendFile("/Users/prasanthksp/Documents/RAMPeX-Parent-Folder/Trainings/SJIT2025MERN/HTML_CSS/index.html")
-})
-
+app.use(express.json())
 const PORT = 3001;
+dotenv.config();
 
+mdb
+  .connect(process.env.MONGODB_URL) // if it doesn't connect with localhost replace it with 127.0.0.1 ip address
+  .then(() => {
+    console.log("MDB Connection Successful");
+  })
+  .catch((err) => {
+    console.log("Check your connection String", err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to Backend</h1>");
+});
+app.get("/static", (req, res) => {
+  res.sendFile(
+    "/Users/prasanthksp/Documents/RAMPeX-Parent-Folder/Trainings/SJIT2025MERN/HTML_CSS/index.html"
+  );
+});
+
+app.post("/signup", (req, res) => {
+  try {
+    console.log(req.body);
+    const { firstName, lastName, email, password, phoneNumber } = req.body;
+    const newSignup = new Signup({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+      phoneNumber: phoneNumber,
+    });
+    newSignup.save();
+    console.log("Signup Successful");
+    res.status(201).json({ message: "Signup Successful", isSignUp: true });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "Signup Unsuccessful", isSignUp: false });
+  }
+});
 app.listen(PORT, () => {
   console.log("Server Started Successfully");
 });
