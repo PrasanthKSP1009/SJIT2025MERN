cf6154e875f3702e31ea78f2af0e963c28789cf0
Prasanth K S <ksprasanthksp10@gmail.com>
2025-02-27 08:14:16 +0530

    Login Completed

--- sjitFrontend/src/components/FunctionalComponents/Login.jsx
+++ sjitFrontend/src/components/FunctionalComponents/Login.jsx
@@ -1,17 +1,53 @@
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    var req = await axios.post("https://sjit2025mern.onrender.com/login", {
+      email,
+      password,
+    });
+    var isLoginSuccessful = req.data.isLoggedIn;
+    if (isLoginSuccessful) {
+      alert(req.data.message);
+      navigate("/");
+    } else {
+      alert(req.data.message);
+    }
+  };
   return (
     <section>
       <h1>Login</h1>
-      <form>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" required /> <br />
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" required /><br />
+      <form onSubmit={handleLogin}>
+        Email:{" "}
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <br /> <br />
+        Password:{" "}
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <br /><br />
         <button type="submit">Login</button>
       </form>
-      <p>Create an account?<Link to='/signup'>Signup</Link></p>
+      <p>
+        Create an account?<Link to="/signup">Signup</Link>
+      </p>
     </section>
   );
 }
