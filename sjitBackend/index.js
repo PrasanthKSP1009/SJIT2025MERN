8cbe474f157c56fd47180f05e377f58f78501794
Prasanth K S <ksprasanthksp10@gmail.com>
2025-02-27 09:23:56 +0530

    CORS Restriction

--- sjitBackend/index.js
+++ sjitBackend/index.js
@@ -5,7 +5,9 @@ const bcrypt = require("bcrypt");
 const cors = require('cors')
 const Signup = require("./models/signupSchema");
 const app = express();
-app.use(cors())
+app.use(cors({
+  origin:"https://sjit-mern.vercel.app/"
+}))
 app.use(express.json());
 const PORT = 3001;
 dotenv.config();
